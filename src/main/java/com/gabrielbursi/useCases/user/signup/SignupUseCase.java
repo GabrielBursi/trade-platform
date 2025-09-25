558130cb00953a13e20d49da21a225f793c2dd7a
Gabriel Bursi <gabriellbursii@gmail.com>
2025-09-24 22:14:15 -0300

    feat(user): add email uniqueness check on signup
    - Added a check in SignupUseCase to ensure that a new user cannot be created with an email that already exists in the system.
    - The UserRepository interface was extended with a findByEmail method to support this feature.

--- src/main/java/com/gabrielbursi/useCases/user/signup/SignupUseCase.java
+++ src/main/java/com/gabrielbursi/useCases/user/signup/SignupUseCase.java
@@ -1,15 +1,28 @@
 package com.gabrielbursi.useCases.user.signup;
 
 import com.gabrielbursi.domain.user.User;
+import com.gabrielbursi.repository.user.UserRepository;
 
 public class SignupUseCase {
+
+    private final UserRepository userRepository;
+
+    public SignupUseCase(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public OutputSignup execute(InputSignup input) {
+        var existingUser = userRepository.findByEmail(input.email());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("User already exists");
+        }
         var user = User.create(
                 input.firstName(),
                 input.lastName(),
                 input.email(),
                 input.cpf(),
                 input.password());
+        userRepository.save(user);
         return new OutputSignup(user.getId().getValue());
     }
 }
